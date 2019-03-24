2c30799d37f430b5a46196921a38431f25149cc7
anthony.souza <anthonys.souza@outlook.com>
2019-03-24 12:05:46 -0300

    Mudançã modelo de Pop

--- backend/src/models/Pop.js
+++ backend/src/models/Pop.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
 
 const PopSchema = new mongoose.Schema({
-  Nome: String
+  Nome: String,
+  Fechado: {
+    type: Boolean,
+    default: false,
+  },
+
 });
 
 module.exports = mongoose.model('Pop', PopSchema);
