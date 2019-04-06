b9ba852166eafde9617620eb5302c32a1ab718f1
anthony.souza <anthonys.souza@outlook.com>
2019-04-06 00:19:34 -0300

    Final neighboors method

--- backend/src/controllers/EnlaceController.js
+++ backend/src/controllers/EnlaceController.js
@@ -16,9 +16,7 @@ module.exports = {
   },
 
   async neighboors({params: {Nome}}, res){
-    //console.log(req)
-    //const {NomePop} = await Pop.find({'Nome': Nome}, '-__v');
-    const neighboors = await Enlace.find({'P1': Nome},  '-__v' )//.find({'P2': Nome}, '-__v')
+    const neighboors = await Enlace.find({'P1': Nome},  '-__v' )
     const neighboors_P2 = await Enlace.find({'P2': Nome},  '-__v')
     neighboors_P2.forEach(enlace => {
       neighboors.push(enlace)
